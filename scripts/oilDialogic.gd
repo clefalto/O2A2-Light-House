4b4b47aa8507438a0e0d0c0dfe67f4ee4150f1db
Haocheng Mai <lightwizard42@gmail.com>
2025-07-18 14:26:42 -0700

    im going crazy

--- scripts/oilDialogic.gd
+++ scripts/oilDialogic.gd
@@ -2,11 +2,16 @@ extends Control
 
 @export var night: int = 1
 
+func _ready() -> void:
+	pass
+
 func _input(event: InputEvent):
 	if Dialogic.current_timeline != null:
 		return
 	
 	if Input.is_action_just_pressed("dialogic_default_action"):
-		Dialogic.start('oil')
+		var dialogueOil = Dialogic.start('oil')
+		get_node("/root/OilMinigame").add_child(dialogueOil)
+		# dialogueOil.get_child()
 		
 		# get_viewport().set_input_as_handled()
