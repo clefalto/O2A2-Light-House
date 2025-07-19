fc881d3cd097314cb2f47d3be1898d23a2af431e
Haocheng Mai <lightwizard42@gmail.com>
2025-07-19 12:43:17 -0700

    im workin on it i swear-

--- scripts/oilDialogic.gd
+++ scripts/oilDialogic.gd
@@ -7,9 +7,12 @@ func _ready() -> void:
 
 func _input(event: InputEvent):
 	if Dialogic.current_timeline != null:
+		#var oilCount = get_node("/root/Dialogic").get_variable("NumOil")
+		#oilCount-=1
 		return
 	
 	if Input.is_action_just_pressed("dialogic_default_action"):
+		print("wharg")
 		var dialogueOil = Dialogic.start('oil')
 		get_node("/root/OilMinigame").add_child(dialogueOil)
 		# dialogueOil.get_child()
